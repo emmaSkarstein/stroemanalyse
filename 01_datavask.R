a67326c737cf0a68e68117a4c50b6184c2c0e8d7
emmaSkarstein <36135547+emmaSkarstein@users.noreply.github.com>
2020-07-20 14:24:49 +0200

    Some changes

--- 01_datavask.R
+++ 01_datavask.R
@@ -9,28 +9,42 @@ library(readxl)
 library(here)
 
 
-load_from_xls <- function(month){
-  filename <- paste0(month, "_strom.xlsx")
-  data <- read_excel(filename)
+load_from_xls <- function(year, month){
+  year = 19
+  month = "09"
+  filename <- paste0("original_files/", year, "_", month, "_strom.csv")
+  data <- read.csv(filename)
   data <- data[2:nrow(data),]
   data$time <- as.character(data$Column1)
   data$time <- as.POSIXct(data$time, format = "%d.%m.%Y %H:%M")
   colnames(data) <- c("Fra", "Til", "forbruk", "time")
-  data$forbruk <- as.numeric(data$forbruk)
+  forbruk <- as.character(data$forbruk)
+  forbruk <- as.numeric(forbruk)
+  data$forbruk <- as.numeric(levels(forbruk))[forbruk]
   data <- select(data, time, forbruk)
-  data$hour <- hour(data$time)
-  data$day <- day(data$time)
-  data$month <- month(data$time)
-  data$year <- year(data$time)
-  saveRDS(data, here("data", paste0(month, "strom.rds")))
+  #data$hour <- hour(data$time)
+  #data$day <- day(data$time)
+  #data$month <- month(data$time)
+  #data$year <- year(data$time)
+  saveRDS(data, here("data/", paste0(year, "_", month, "_strom.rds")))
   return(data)
 }
 
-merge_months <- function(months){
-  data <- readRDS(paste0(months[1], "strom.rds"))
+
+merge_months <- function(months, year){
+  filename <- paste0("csv_data/", year, "_", months[1], "_strom.csv")
+  data <- read.csv(filename, sep = ";", dec = ",")
   for(i in 2:length(months)){
-    data <- rbind(data, readRDS(paste0(months[i], "strom.rds")))
+    filename <- paste0("csv_data/", year, "_", months[i], "_strom.csv")
+    data <- rbind(data, read.csv(filename, sep = ";", dec = ","))
   }
+  return(data)
+}
+
+clean_df <- function(data){
+  data <- data %>% select(Fra, KWH.60.Forbruk)
+  colnames(data) <- c("tid", "forbruk")
+  data$tid <- as.POSIXct(data$tid, format = "%d.%m.%Y %H:%M")
   return(data)
 }
 
--- 02_plotting.R
+++ 02_plotting.R
@@ -5,11 +5,13 @@
 # Load data 
 source("01_datavask.R")
 
-data <- load_from_xls("01")
-
+#data <- load_from_xls("01")
+#readRDS("data/01strom.rds")
 
 data <- merge_months(c("01", "02", "03", "04", "05", "06", "07", "08"))
+data$time <- as.Date(data$time)
 
+summary(data$forbruk)
 
 ggplot(data, aes(x = time, y = forbruk)) + 
   geom_line()
@@ -20,4 +22,36 @@ ggplot(data, aes(x = time, y = forbruk))+
 
 summary(data)
 
+# Total amount spent per day
+data %>% aggregate(by = list("month"), nfrequency = 12, FUN = sum)
 
+
+# Convert to time series object
+library(xts)
+data_xts <- as.xts(data[-1], order.by = data[[1]], dateFormat = "Date")
+periodicity(data_xts)
+test <- to.period(data_xts, period="day")
+
+day_ep <- endpoints(data_xts, on="days")  
+month_ep <- endpoints(data_xts, on="months") 
+dayly_sum <- period.apply(data_xts, INDEX = day_ep, FUN = sum)
+monthly_sum <- period.apply(data_xts, INDEX = month_ep, FUN = sum)
+plot(dayly_sum)
+plot(monthly_sum)
+
+ggplot(data_xts, aes(x = Index, y = forbruk)) +
+  geom_col() +
+  scale_x_date(date_labels = "%b", date_breaks = "1 month") + xlab("")
+
+
+str(data_xts)
+
+daily_data <- data %>%
+  group_by(day, month) %>%
+  summarise(sum_forb = sum(forbruk))
+
+
+library(forecast)
+d.arima <- auto.arima(data_xts)
+d.forecast <- forecast(d.arima, level = c(95), h = 50)
+autoplot(d.forecast)
